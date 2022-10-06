ada96bf9516a47871d2136d116a48cee7d02d49b
Radmir <radmir_96@mail.ru>
2022-10-06 18:59:20 +0500

    Error in Row.jsx map

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Main from '../components/Main/Main'
+import Row from '../components/Row/Row'
+import requests from '../utils/Requests'
+
+const Home = () => {
+    return (
+        <>
+            <Main />
+            <Row title='Upcoming' fetchURL={requests.requestUpcoming} />
+            <Row title='Popular' fetchURL={requests.requestPopular} />
+            <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+            <Row title='Latest' fetchURL={requests.requestLatest} />
+            <Row title='Now Playing' fetchURL={requests.requestNowPlaying} />
+        </>
+    )
+}
+
+export default Home
---
+++ src/utils/Requests.js
@@ -0,0 +1,11 @@
+const key = 'f3dc3cd24f851e9d22e9abc41eb77c67'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestLatest: `https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`
+}
+
+export default requests
